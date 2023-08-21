c545ade739e33d962bae4e446924b68c05567667
Yuho <yuho@ref.finance>
2023-08-21 23:34:14 +0800

    custom modal

--- components/CustomModal/CustomModal.tsx
+++ components/CustomModal/CustomModal.tsx
@@ -1,18 +1,52 @@
-import { Modal as MUIModal, Box } from "@mui/material";
-import { Wrapper } from "./style";
+import styled from "styled-components";
+import { twMerge } from "tailwind-merge";
+import React from "react";
+import Portal from "./portal";
+import { BaseProps } from "../../interfaces/common";
 
-const CustomModal = ({ children, isOpen, onClose }) => {
+type onClickHandler = (event: React.MouseEvent<HTMLElement>) => any;
+
+interface Props extends BaseProps {
+  isOpen: boolean;
+  size?: string;
+  width?: number;
+  canScroll?: boolean;
+  onClose: onClickHandler;
+  onOutsideClick?: onClickHandler;
+}
+
+const CustomModal = ({
+  children,
+  isOpen,
+  onClose,
+  onOutsideClick,
+  size,
+  width,
+  canScroll,
+}: Props) => {
+  const styles: { width?: number } = {};
+  if (width) {
+    styles.width = width;
+  }
   return (
-    <MUIModal open={isOpen} onClose={onClose}>
-      <Wrapper
-        sx={{
-          "& *::-webkit-scrollbar": {},
-        }}
-      >
-        {children}
-      </Wrapper>
-    </MUIModal>
+    <Portal>
+      <StyledWrapper className={twMerge("modal fade", isOpen && "show")}>
+        <div className="overlay" onClick={onOutsideClick} />
+        <div className={twMerge("modal-dialog background-paper", size && `modal-${size}`)}>
+          <div className="modal-content" style={styles}>
+            <div className={twMerge("modal-body", canScroll && "modal-body-scroll")}>
+              {children}
+            </div>
+          </div>
+        </div>
+      </StyledWrapper>
+    </Portal>
   );
 };
+
+const StyledWrapper = styled.div`
+  .custom-table-th {
+  }
+`;
 
 export default CustomModal;
