c0d45ebb48ffb8a019acb7d0fdaa9f6e790b9ce4
Yuho <yuho@ref.finance>
2023-08-21 22:22:37 +0800

    custom table component

--- components/CustomTable/CustomTable.tsx
+++ components/CustomTable/CustomTable.tsx
@@ -1,26 +1,105 @@
 import styled from "styled-components";
-import { BaseProps } from "../../interfaces/common";
+import React from "react";
 
-// todo: use json structure instead of children
-const CustomTable = ({ children }: BaseProps) => {
-  return <StyledTable className="custom-table">{children}</StyledTable>;
+type columnType = {
+  id?: string;
+  header: string;
+  accessorKey?: string;
+  cell?: any;
 };
 
-const StyledTable = styled.table`
+interface Props {
+  columns: Array<columnType>;
+  data: Array<any>;
+  actionRow?: React.ReactNode;
+}
+
+const CustomTable = ({ columns, data, actionRow }: Props) => {
+  const headers = columns?.map((d) => d.header);
+  const headerNode = (
+    <div className="custom-table-thead">
+      <div className="custom-table-tr">
+        {headers?.map((d) => (
+          <div key={d} className="custom-table-th text-gray-400">
+            {d}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+
+  const bodyNodes = data?.map((d, i) => {
+    const tdNode = columns?.map((col) => {
+      let content = null;
+      if (typeof col.cell === "function") {
+        content = col.cell({
+          originalData: d,
+        });
+      } else if (col.accessorKey) {
+        content = d[col.accessorKey];
+      }
+      return (
+        <div className="custom-table-td" key={col.id || col.header}>
+          {content}
+        </div>
+      );
+    });
+    return (
+      <div className="custom-table-row" key={i}>
+        <div className="custom-table-tr">{tdNode}</div>
+        {actionRow && <div className="custom-table-action">{actionRow}</div>}
+      </div>
+    );
+  });
+
+  return (
+    <StyledTable className="custom-table">
+      <div className="custom-table-thead">{headerNode}</div>
+      <div className="custom-table-tbody">{bodyNodes}</div>
+    </StyledTable>
+  );
+};
+
+const StyledTable = styled.div`
   width: 100%;
 
-  thead {
+  .custom-table-tr {
+    display: flex;
+  }
+
+  .custom-table-th,
+  .custom-table-td {
+    flex: 1;
+  }
+
+  .custom-table-thead {
     border-bottom: 1px solid #31344d;
 
-    th {
+    .custom-table-th {
       text-align: left;
       padding: 10px 5px;
     }
   }
 
-  tbody {
-    td {
+  .custom-table-tbody {
+    .custom-table-td {
       padding: 15px 5px;
+    }
+  }
+
+  .custom-table-action {
+    //display: none;
+    //height: 0;
+    //overflow: hidden;
+    //transition: all ease-in-out 0.3ms;
+  }
+
+  .custom-table-row {
+    &:hover {
+      .custom-table-action {
+        display: block;
+        //height: auto;
+      }
     }
   }
 `;
