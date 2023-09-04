49757d911dcdcd4bd4c20333f2d89c8fd4508309
nature.xie <nature@ref.finance>
2023-09-04 17:10:23 +0800

    add top loader bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Head from "next/head";
 import { Provider } from "react-redux";
 import type { AppProps } from "next/app";
@@ -9,6 +9,8 @@ import posthogJs from "posthog-js";
 import { useIdle, useInterval } from "react-use";
 
 import "../styles/global.css";
+import LoadingBar from "react-top-loading-bar";
+import { useRouter } from "next/router";
 import { store, persistor } from "../redux/store";
 import { FallbackError, Layout, Modal } from "../components";
 import { posthog, isPostHogEnabled } from "../utils/telemetry";
@@ -58,8 +60,21 @@ const Init = () => {
 };
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const [progress, setProgress] = useState(0);
+  const router = useRouter();
+  useEffect(() => {
+    router.events.on("routeChangeComplete", () => {
+      setProgress(100);
+    });
+  }, []);
   return (
     <ErrorBoundary fallback={FallbackError}>
+      <LoadingBar
+        color="#D2FF3A"
+        height={3}
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
+      />
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <Head>
