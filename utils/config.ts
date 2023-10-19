f6f89c710e78eca129792e5268bcb137e08359df
nature.xie <nature@ref.finance>
2023-10-19 21:26:04 +0800

    update

--- utils/config.ts
+++ utils/config.ts
@@ -41,7 +41,10 @@ const getConfig = (env: string = defaultNetwork) => {
           "usdt.tether-token.near",
           "17208628f84f5d6ad33f0da3bbbeb27ffcb398eac501a31bd6ad2011e36133a1",
         ],
-        NEW_TOKENS: ["17208628f84f5d6ad33f0da3bbbeb27ffcb398eac501a31bd6ad2011e36133a1"],
+        NEW_TOKENS: [
+          "usdt.tether-token.near",
+          "17208628f84f5d6ad33f0da3bbbeb27ffcb398eac501a31bd6ad2011e36133a1",
+        ],
       } as unknown as ConnectConfig;
 
     case "development":
