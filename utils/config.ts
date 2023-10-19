2807dc32c748208b513e7f89e0577ce3b9d783a0
nature.xie <nature@ref.finance>
2023-10-19 20:43:16 +0800

    add new label

--- utils/config.ts
+++ utils/config.ts
@@ -37,6 +37,11 @@ const getConfig = (env: string = defaultNetwork) => {
         SPECIAL_REGISTRATION_TOKEN_IDS: [
           "17208628f84f5d6ad33f0da3bbbeb27ffcb398eac501a31bd6ad2011e36133a1",
         ],
+        NATIVE_TOKENS: [
+          "usdt.tether-token.near",
+          "17208628f84f5d6ad33f0da3bbbeb27ffcb398eac501a31bd6ad2011e36133a1",
+        ],
+        NEW_TOKENS: ["17208628f84f5d6ad33f0da3bbbeb27ffcb398eac501a31bd6ad2011e36133a1"],
       } as unknown as ConnectConfig;
 
     case "development":
@@ -52,6 +57,8 @@ const getConfig = (env: string = defaultNetwork) => {
         SPECIAL_REGISTRATION_TOKEN_IDS: [
           "3e2210e1184b45b64c8a434c0a7e7b23cc04ea7eb7a6c3c32520d03d4afcb8af",
         ],
+        NATIVE_TOKENS: ["usdc.fakes.testnet"],
+        NEW_TOKENS: ["usdc.fakes.testnet"],
       } as unknown as ConnectConfig;
     case "betanet":
       return {
